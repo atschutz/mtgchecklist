baac2c278c5a486891a0a103326a72399e9084c5
Alex Schutz <alexandertschutz@gmail.com>
2020-01-01 20:06:43 -0500

    Small tweaks to progress bar

--- progresscheck.js
+++ progresscheck.js
@@ -1,11 +1,11 @@
 const checkBoxes = document.querySelectorAll('input[type=checkbox]')
-const progressBar = document.getElementById('progressbar')
-const progressLabel = document.getElementById('progresslabel')
+const progressBar = document.querySelector('#progressbar')
+const progressLabel = document.querySelector('#progresslabel')
 
 checkBoxes.forEach(box => {
     box.addEventListener('change', () => {
-        let checked = document.querySelectorAll('input:checked')
-        let percent = Math.round(checked.length / checkBoxes.length * 100)
+        const checked = document.querySelectorAll('input:checked')
+        const percent = Math.round(checked.length / checkBoxes.length * 100)
         progressBar.style.width = percent + '%'
         progressLabel.innerHTML = percent + '% Complete'
     })
