8ca09e45f87e8a91939b82fdec17a59ed7cdb198
Alex Schutz <alexandertschutz@gmail.com>
2020-01-01 20:26:47 -0500

    Added strikethrough when an item is checked off

--- progresscheck.js
+++ progresscheck.js
@@ -6,7 +6,15 @@ checkBoxes.forEach(box => {
     box.addEventListener('change', () => {
         const checked = document.querySelectorAll('input:checked')
         const percent = Math.round(checked.length / checkBoxes.length * 100)
+        const label = document.querySelector('label[for=' + box.name + ']')
+
         progressBar.style.width = percent + '%'
         progressLabel.innerHTML = percent + '% Complete'
+
+        if (box.checked) {
+            label.style.setProperty('text-decoration', 'line-through')
+        } else {
+            label.style.setProperty('text-decoration', '')
+        }
     })
 })
