c52abdbfdfcc89955d960dae7c2ff50d38bb81b5
Alex Schutz <alexandertschutz@gmail.com>
2020-01-07 23:51:45 -0500

    Early card name checking. Need to fix margins and stuff.

--- scripts/builddecklist.js
+++ scripts/builddecklist.js
@@ -2,6 +2,7 @@ const container = document.querySelector('#listcontainer')
 const textbox = document.querySelector('#decklist')
 const button = document.querySelector('#getlistbutton')
 const template = document.querySelector('#checkboxtemplate')
+const errorMessage = document.querySelector('#errormessage')
 
 textbox.addEventListener('click', () => {
     if (textbox.value == textbox.innerHTML) {
@@ -18,19 +19,7 @@ button.addEventListener('click', () => {
 
     cardList.forEach((card, index) => {
         card = card.trim()
-
-        if (card !== ''){
-            let clone = document.importNode(template.content, true)
-            let label = clone.querySelector('label')
-            let checkbox = clone.querySelector('input[type=checkbox]')
-            
-            label.htmlFor = 'cb' + index
-            label.innerHTML = card
-
-            checkbox.name = 'cb' + index
-
-            container.appendChild(clone)
-        }
+        getCardData(card, index)
     })
 
     textbox.value = textbox.innerHTML
@@ -38,11 +27,12 @@ button.addEventListener('click', () => {
     progressBarListener()
 })
 
-//get card data and prices
-function getCardData(cardName){
-    const base = 'https://api.scryfall.com';
-    const api = '/cards/named?fuzzy=';
-        
+//get card data
+function getCardData(cardName, id){
+    const base = 'https://api.scryfall.com'
+    const api = '/cards/named?fuzzy='
+    let name = ""
+
     fetch(base + api + cardName.replace(/\s/g, "+"))
     .then(res => {
         return res.json()
@@ -51,10 +41,34 @@ function getCardData(cardName){
         //res is now a json!
         if (res.status === 404)
         {
+            console.log(cardName + " not a card in getCardData")
 
-            throw new Error(item + " doesnt exist")
+            errorMessage.style.display = "block"
+
+            let node = document.createElement("LI");
+            let textnode = document.createTextNode(cardName);
+            
+            node.appendChild(textnode);                             
+            errorMessage.appendChild(node); 
         } else {
-            console.log(res.oracle_text)
+            name = res.name
+
+            console.log("we have " + name)
+
+            let clone = document.importNode(template.content, true)
+            let label = clone.querySelector('label')
+            let checkbox = clone.querySelector('input[type=checkbox]')
+            
+            // give "for" for this label a unique id
+            label.htmlFor = 'cb' + id
+
+            // set label to card name
+            label.innerHTML = name
+
+            // give this checkbox a name that matches its label
+            checkbox.name = 'cb' + id
+
+            container.appendChild(clone)
         }
     })
     .catch(e => {
